c2d2577e2274f1a2cffcb0c2e45a345b025f000d
nipanimaju <zuc.pawel@gmail.com>
2021-02-18 00:09:46 +0100

    chart

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 
+import {ChartComponent} from 'bar-chart-simple'
 import "./home.css";
 
 const link = `http://localhost:3001/`;
@@ -16,30 +17,40 @@ const Home = () => {
         setHomeData(data);
         setIsLoading(false)
     }
+    
+    let data = () => {
+      let dataArr = [];
+        if (isLoading) {
+            return null
+        } else {
+            homeData.map((item, i) => {
+                dataArr.push({"data_category": item.number, "data_value": item.numberOfCalls})
+            })
+        }
+        return dataArr;
+      }
+        return(isLoading ? null : (
+            <>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Phone number</th>
+                            <th>Number of calls</th>
+                            <th>Last call details</th>
+                        </tr>
+                    </thead>
+                    <tbody>{homeData.map((item, i) => (<tr key={i}>
+                                <td>{item.number}</td>
+                                <td>{item.numberOfCalls}</td>
+                                <td>{item.agentName} / {item.lastCall.slice(11, -8)}</td>
+                            </tr>
+                        ))}
+                        </tbody>
+                </table>
+                      <ChartComponent   data={data()}
+                      chart_type="bar_chart"/>
+                  </>
+        ));
+    };
 
-    return (
-        isLoading ? null : ( <>    
-          <table>
-            <thead>
-              <tr>
-                <th>Phone number</th>
-                <th>Number of calls</th>
-                <th>Last call details</th>
-              </tr>
-            </thead>
-            <tbody>
-              {homeData.map((item, i) => (
-                <tr key={i}>
-                  <td>{item.number}</td>
-                  <td>{item.numberOfCalls}</td>
-                  <td>{item.agentName} / {item.lastCall.slice(11,-8)}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-          
-      </>)
-    );
-};
-
-export default Home;
+    export default Home;
